f82eaaca6c9a362651e3f434d8daf167e9c1a72a
Obellay <oceanebellay85340@gmail.com>
2025-03-29 16:58:25 +0100

    pagination sur page d'accueul ok, a mettre en place sur les autres page

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,18 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface; 
 use App\Entity\Book;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, PaginatorInterface $paginator): Response
     {
         // Récupération des livres via le repository
         $bookRepository = $entityManager->getRepository(Book::class);
-        $books = $bookRepository->findAll();
+        $queryBuilder = $bookRepository->createQueryBuilder('b');
+        $page = $request->query->getInt('page', 1); // Récupère le numéro de page
+        $books = $paginator->paginate(
+            $queryBuilder,   // La requête à paginer
+            $page,           // Le numéro de page
+            20              // Nombre d'éléments par page
+        );
+
         return $this->render('home/index.html.twig', [
         'books' => $books,
+        
     ]);
     }
 }
