7a7b17e110c99359a5ccc38f05cfae49f51ad4f2
Obellay <oceanebellay85340@gmail.com>
2025-04-09 15:32:36 +0200

    modif login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,21 +6,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Form\LoginFormType;
+use Symfony\Component\HttpFoundation\Request;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
-    {
-      
-        $error = $authenticationUtils->getLastAuthenticationError();
-        $lastUsername = $authenticationUtils->getLastUsername();
+public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
+{
+    $error = $authenticationUtils->getLastAuthenticationError();
+    $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', [
-            'last_username' => $lastUsername,
-            'error' => $error,
-        ]);
-    }
+    $form = $this->createForm(LoginFormType::class, [
+        'email' => $lastUsername,
+    ]);
+
+    return $this->render('security/login.html.twig', [
+        'loginForm' => $form,
+        'error' => $error,
+    ]);
+}
 
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
